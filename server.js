6f20d7f994f1a00d8a7c60284dbc5a6278017ced
jack battle <jackonno78@hotmail.com>
2021-02-07 20:07:03 -0600

    added start function

--- server.js
+++ server.js
@@ -12,4 +12,53 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  startFunction();
 });
+
+function startFunction() {
+    inquirer
+      .prompt({
+        type: "list",
+        choices: [
+          "Add department",
+          "Add role",
+          "Add employee",
+          "View departments",
+          "View roles",
+          "View employees",
+          "Update employee role",
+          "Quit"
+        ],
+        message: "What would you like to do?",
+        name: "option"
+      })
+      .then(function(result) {
+        console.log("You chose " + result.option);
+  
+        switch (result.option) {
+          case "Add department":
+            addDepartment();
+            break;
+          case "Add role":
+            addRole();
+            break;
+          case "Add employee":
+            addEmployee();
+            break;
+          case "View departments":
+            viewDepartment();
+            break;
+          case "View roles":
+            viewRoles();
+            break;
+          case "View employees":
+            viewEmployees();
+            break;
+          case "Update employee role":
+            updateEmployee();
+            break;
+          default:
+            quit();
+        }
+      });
+}
