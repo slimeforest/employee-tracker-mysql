cda890f6779dff22826f4c9347be63969ff1af4d
jack battle <jackonno78@hotmail.com>
2021-02-07 21:04:19 -0600

    added add functions for department, role, and employee

--- server.js
+++ server.js
@@ -61,4 +61,79 @@ function startFunction() {
             quit();
         }
       });
-}
+};
+
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        message: "What is the name of the department?",
+        name: "deptName"
+    }).then(function(answer){
+        connection.query("INSERT INTO department (name) VALUES (?)", [answer.deptName] , function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            startFunction();
+    })
+    })
+};
+
+function addRole() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the role?",
+          name: "roleName"
+        },
+        {
+          type: "input",
+          message: "What is the salary for this role?",
+          name: "salaryTotal"
+        },
+        {
+          type: "input",
+          message: "What is the department id number?",
+          name: "deptID"
+        }
+      ])
+      .then(function(answer) {
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.roleName, answer.salaryTotal, answer.deptID], function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          startFunction();
+        });
+      });
+};
+
+function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the first name of the employee?",
+          name: "eeFirstName"
+        },
+        {
+          type: "input",
+          message: "What is the last name of the employee?",
+          name: "eeLastName"
+        },
+        {
+          type: "input",
+          message: "What is the employee's role id number?",
+          name: "roleID"
+        },
+        {
+          type: "input",
+          message: "What is the manager id number?",
+          name: "managerID"
+        }
+      ])
+      .then(function(answer) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.eeFirstName, answer.eeLastName, answer.roleID, answer.managerID], function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          startFunction();
+        });
+      });
+};
